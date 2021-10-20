5f82da3ade37deae47169ff90c3fe91947e24134
Jonathan E. Hardis <jhardis@me.com>
2021-10-19 22:30:16 -0400

    End of Part 1

---
+++ RPS/GameState.swift
@@ -0,0 +1,15 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Jonathan E. Hardis on 10/19/21.
+//
+
+import Foundation
+
+enum GameState {
+    case start
+    case win
+    case lose
+    case draw
+}
--- RPS/Sign.swift
+++ RPS/Sign.swift
@@ -7,6 +7,17 @@
 
 import Foundation
 
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    if sign == 0 {
+        return .rock
+    } else if sign == 1 {
+        return .paper
+    } else {
+        return .scissors
+    }
+}
+
 enum Sign {
     case rock
     case paper
@@ -20,6 +31,38 @@ enum Sign {
             return "✋"
         case .scissors:
             return "✌️"
+        }
+    }
+    
+    func faceoff(_ opponent: Sign) -> GameState {
+        switch self {
+        case .rock:
+            switch opponent {
+            case .rock:
+                return .draw
+            case .paper:
+                return .lose
+            case .scissors:
+                return .win
+            }
+        case .paper:
+            switch opponent {
+            case .rock:
+                return .win
+            case .paper:
+                return .draw
+            case .scissors:
+                return .lose
+            }
+        case .scissors:
+            switch opponent {
+            case .rock:
+                return .lose
+            case .paper:
+                return .win
+            case .scissors:
+                return .draw
+            }
         }
     }
 }
