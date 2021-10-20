3436c36118b1202d1d617d929125f1069ff87b5f
Jonathan E. Hardis <jhardis@me.com>
2021-10-19 23:09:41 -0400

    End of Part 2

--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var appSign: UILabel!
+    
+    @IBOutlet weak var gameStatus: UILabel!
+    
+    @IBOutlet weak var rockButton: UIButton!
+    
+    @IBOutlet weak var paperButton: UIButton!
+    
+    @IBOutlet weak var scissorsButton: UIButton!
+    
+    @IBOutlet weak var playagainButton: UIButton!
+    
+    @IBAction func rockTouch(_ sender: Any) {
+    }
+    
+    @IBAction func paperTouch(_ sender: Any) {
+    }
+    
+    @IBAction func scissorsTouch(_ sender: Any) {
+    }
+    
+    @IBAction func playTouch(_ sender: Any) {
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
